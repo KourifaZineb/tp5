cc84dbb7baed7f6cc354b0750df414beebcce4e4
Kourifa Zineb <you@example.com>
2025-01-24 20:57:58 +0100

    V1 TP5

---
+++ src/main/java/net/kourifa/tp5devops/Tp5devopsApplication.java
@@ -0,0 +1,13 @@
+package net.kourifa.tp5devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5devopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp5devopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/kourifa/tp5devops/Tp5devopsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kourifa.tp5devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp5devopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
